26b6ce24fa9be6a38808f58b6cb43cb77058001f
Stefan Arentz <stefan@arentz.ca>
2023-02-07 14:57:12 -0500

    Rust: scrabble-score

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,17 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    let mut score = 0;
+    for c in word.chars() {
+        score += match c.to_ascii_uppercase() {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        };
+    }
+    score
+}
