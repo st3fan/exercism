309275688cede46fcc6d1003d5c632e9cc0eec3d
Stefan Arentz <stefan@arentz.ca>
2023-02-22 13:33:30 -0500

    rust: perfect-numbers

---
+++ rust/perfect-numbers/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Classification {
+    Abundant,
+    Perfect,
+    Deficient,
+}
+
+fn factors(num: u64) -> Vec<u64> {
+    (1..=num / 2).into_iter().filter(|n| num % n == 0).collect()
+}
+
+pub fn classify(num: u64) -> Option<Classification> {
+    if num == 0 {
+        return None;
+    }
+
+    let sum: u64 = factors(num).iter().sum();
+
+    println!("Factors: {:?}", factors(num));
+
+    if sum == num {
+        Some(Classification::Perfect)
+    } else {
+        if sum < num {
+            Some(Classification::Deficient)
+        } else {
+            Some(Classification::Abundant)
+        }
+    }
+}
